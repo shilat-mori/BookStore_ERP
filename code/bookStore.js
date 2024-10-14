b66cc9501d553b351e06aa2039e1c7d8a55994b6
Shilat Mori <7684129@gmail.com>
2024-10-14 11:46:06 +0300

    complete pagination style and filling table

--- code/bookStore.js
+++ code/bookStore.js
@@ -4,32 +4,37 @@ var booksStorage
 var maxPage
 var pagination = 1
 var bookId = 0
+var paging_range = [1,2,3]
 
-load_page()
-function load_page(){
-    let books = load_books()
-    console.log(books);
+loading_data()
+
+function loading_data(){
+    load_books()
+    paging(0)
+    eventListenersSetting()
     
-    console.log(load_books());
-    console.log(books); 
-    fill_table(books)
 }
 
 function load_books(){
-    let books
     localStorage.setItem('booksStorage', JSON.stringify(bookStore.books))
-    booksStorage = JSON.parse(localStorage.getItem('booksStorage')||'[]')
-    maxPage = booksStorage.length/booksShown
-    console.log(booksStorage);
-    
-    return booksStorage.splice((booksShown*(pagination-1)),booksShown)
+    booksStorage = JSON.parse(localStorage.getItem('booksStorage')||'[]')   
+    maxPage = booksStorage.length/booksShown    
 }
 
-function fill_table(books){
-    let tbody, row, tr,td,btn
+function fill_table(){
+    let tbody, row, tr,td,btn, booksToFill
+    
+    //empty the table
     tbody = document.getElementById('tableBody')
+    while (tbody.children.length > 1) {
+        tbody.children[1].remove();
+    }
+   
+    //multiple the first row model and fill it
     row = document.getElementById('row-')
-    for(let i in books){
+    booksToFill = booksStorage.slice((booksShown*(pagination-1)),(booksShown*(pagination-1))+booksShown)
+    
+    for(let i in booksToFill){
         //multiple the node including its innerHTML nodes
         tr = row.cloneNode(true)
         
@@ -39,17 +44,17 @@ function fill_table(books){
         //id
         td = tr.querySelector('#column-id-')
         td.id = 'column-id-'+i
-        td.textContent = books[i].id
+        td.textContent = booksToFill[i].id
 
         //title
         td = tr.querySelector('#column-title-')
         td.id = 'column-title-'+i
-        td.textContent = books[i].title
+        td.textContent = booksToFill[i].title
 
         //price
         td = tr.querySelector('#column-price-')
         td.id = 'column-price-'+i
-        td.textContent = books[i].price+'$'
+        td.textContent = booksToFill[i].price+'$'
 
         //action buttons
         //read
@@ -69,7 +74,7 @@ function fill_table(books){
         
         //choose the book
         tr.addEventListener('click', ()=>{
-            show_book_details(books[i])
+            show_book_details(booksToFill[i])
         })   
         //append row to table
         tbody.appendChild(tr)
@@ -105,9 +110,52 @@ function show_book_details(book){
 }
 
 function paging(count){
-    pagination+=(count+pagination<1)?1:(count+pagination>maxPage)?maxPage:count+pagination
+    let page_numbers, indexPage
     page_numbers = document.getElementsByClassName('page-num')
-    for(let i in page_numbers){
-        pag
+
+    //remove the style from last paging
+    indexPage = paging_range.indexOf(pagination)
+    
+    page_numbers[indexPage].classList.add('btn-dark')
+    page_numbers[indexPage].classList.remove('btn-light')
+
+    //paging
+    pagination=(count+pagination<1)?1:(count+pagination>maxPage)?maxPage:pagination+count
+    indexPage = paging_range.indexOf(pagination)
+
+    //switch the pagination nav
+    switch(count){
+        case 0:{
+            break
+        }
+        case 1:{
+            if(indexPage!=-1)break
+            paging_range.push(paging_range[paging_range.length-1]+1)
+            paging_range.shift()
+            indexPage=page_numbers.length-1
+            break
+        }
+        case -1:{
+            if(indexPage!=-1)break
+            paging_range.pop()
+            paging_range.unshift(paging_range[0]-1)
+            indexPage=0
+            break
+        }
     }
-}
+    for(let i in paging_range) page_numbers[i].innerText = paging_range[i]
+    
+    //add style to current paging
+    if(indexPage!=-1) {
+        page_numbers[indexPage].classList.remove('btn-dark')
+        page_numbers[indexPage].classList.add('btn-light')
+    }
+
+    fill_table()
+ }
+
+ function eventListenersSetting(){
+    document.getElementById('prevPages').addEventListener('click', ()=>{paging(-1)});
+    document.getElementById('nextPages').addEventListener('click', ()=>{paging(1)});
+ }
+
